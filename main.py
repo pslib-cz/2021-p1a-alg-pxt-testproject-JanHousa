836364285e7b56c98fa445368056aa21c3fa2674
Raccoon <53347386+JanHousa@users.noreply.github.com>
2022-05-19 09:18:10 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-basic.show_icon(IconNames.HEART)
+basic.show_icon(IconNames.ANGRY)
 def on_forever():
     pass
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-basic.showIcon(IconNames.Heart)
+basic.showIcon(IconNames.Angry)
 basic.forever(function on_forever() {
     
 })
